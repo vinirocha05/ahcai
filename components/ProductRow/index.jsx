3dcd3ebfb15daccaeb3fb43fba3dfb0d75a28760
Vinicius Rocha <vinirocha05@gmail.com>
2022-02-08 09:35:03 -0300

    ajustando carrosel de produtos

--- components/ProductRow/index.jsx
+++ components/ProductRow/index.jsx
@@ -3,13 +3,19 @@ import Image from "next/image"
 import style from '../../styles/ProductRow.module.css'
 
 
-export default function ProductRow({title,photo,about}) {
-  return(
-    <div className={style.images} >
-      <h2>{title}</h2>
-      <div className="productDescription">
-      <div>{about}</div>
-      <Image src={photo} alt="Produto" width='300px' height='250px' />
+export default function ProductRow({ title, photo, about }) {
+  return (
+    <div className={style.productRow}>
+      <div className={style.listArea} >
+        <div className={style.productList}>
+          <div className={style.productListItem}>
+            <h2>{title}</h2>
+            <div className={style.productInfo}>
+              <div className={style.productImage}><Image src={photo} alt="Produto" width='300px' height='250px' /></div>
+              <div className={style.productDescription}>{about}</div>
+            </div>
+            </div>
+        </div>
       </div>
     </div>
   )
