1af930a1f55d5fc5575aa6ec06d2f47ec52dae3e
Vinicius Rocha <vinirocha05@gmail.com>
2022-02-08 19:23:50 -0300

    carrosel produtos funcionando

--- pages/api/produtos.js
+++ pages/api/produtos.js
@@ -15,7 +15,7 @@ export default function handler(req, res) {
       photo: "http://localhost:3000/assets/img/agua_coco.png",
     },
     {
-      title: "Sorvetes",
+      title: "Sorvetes de massa",
       about:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam quaerat alias ipsam tenetur distinctio! Quas autem voluptas in consectetur aspernatur veritatis architecto praesentium iste amet, laboriosam minus, sint dolorum doloremque!",
       photo: "http://localhost:3000/assets/img/sorvete.jpg",
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FaWhatsapp } from 'react-icons/fa'
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa'
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -12,6 +13,9 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [changeHeader, setChangeHeader] = useState(false);
   const [produtos, setProdutos] = useState([]);
+
+  const [x,setX] =useState(300)
+  console.log(x);
 
   const dados = async () => {
     fetch('http://localhost:3000/api/produtos')
@@ -31,6 +35,20 @@ export default function Home() {
     window.addEventListener('scroll', scrollListener);
   }, []);
 
+  const handleClickLeft =() =>{
+    x +=1800
+    if (x > 300) {
+      x = 300;
+    }
+    setX(x);
+  }
+  const handleClickRight = () => {
+    x -=1800
+    if (x < -3600) {
+      x = -3600;
+    }
+    setX(x);
+};
 
   return (
     <>
@@ -69,9 +87,12 @@ export default function Home() {
 
       {/*Terceira sessão Produtos*/}
       <div className={`${style.center} ${style.bgWhite}`} id='inicio'>
-        <div className={style.productContainer}>
+        <button onClick={handleClickLeft}  className={style.buttonLeft}><FaAngleLeft /></button>
+        <button onClick={handleClickRight} className={style.buttonRight}><FaAngleRight /></button>
+
+        <div className={style.productContainer} style={{marginLeft:`${x}px`}} >
           {produtos.map((e, key) => (
-            <ProductRow title={e.title} photo={e.photo} about={e.about} key={key} />
+            <ProductRow title={e.title} photo={e.photo} about={e.about} key={key}/>
           ))}
         </div>
       </div>
@@ -100,8 +121,8 @@ export default function Home() {
             <h2>Onde todo mundo se encontra</h2>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam quaerat alias ipsam tenetur distinctio! Quas autem voluptas in consectetur aspernatur veritatis architecto praesentium iste amet, laboriosam minus, sint dolorum doloremque!</p>
             <div className={style.iconsContainer}>
-            <Link href='https://whatsa.me/5516981584145/?t=Oii!%20Gostaria%20de%20saber%20mais%20sobre%20o%20A%C3%A7ai!' passHref><a><FaWhatsapp/></a></Link>
-            <Link href='https://www.ifood.com.br/delivery/ribeirao-preto-sp/ahcai-delicias-brasileiras-centro/33b49dfc-40cd-4bae-baff-028964ab516d?UTM_Medium=share' passHref><a><span><Image src='/assets/img/ifood-logo.svg' alt='Link para o ifood ' width='65px' height='45px' /></span></a></Link>
+              <Link href='https://whatsa.me/5516981584145/?t=Oii!%20Gostaria%20de%20saber%20mais%20sobre%20o%20A%C3%A7ai!' passHref><a><FaWhatsapp /></a></Link>
+              <Link href='https://www.ifood.com.br/delivery/ribeirao-preto-sp/ahcai-delicias-brasileiras-centro/33b49dfc-40cd-4bae-baff-028964ab516d?UTM_Medium=share' passHref><a><span><Image src='/assets/img/ifood-logo.svg' alt='Link para o ifood ' width='65px' height='45px' /></span></a></Link>
             </div>
           </div>
         </div>
